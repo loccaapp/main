a9270857ec86b2dbb3a37aab95ef22518889ae32
gollumsss <uygarer@yandex.com>
2016-05-30 23:46:07 +0300

    aaaa

--- DAL/src/models/Like.java
+++ DAL/src/models/Like.java
@@ -3,7 +3,7 @@ package models;
 import java.sql.Timestamp;
 
 public class Like {
-	//comment for first checkout222 new version
+	//comment for first checkout222 new versionaaaa
 	public int post_id;
 	public int effecter_user_id;
 	public int user_id;
